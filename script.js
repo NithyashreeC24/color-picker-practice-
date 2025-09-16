10ee73c88381dfe49804703e9532c409fb73bbfe
NITHYASHREE C <nithyashreec24@gmail.com>
2025-09-16 19:39:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let colorpicker = document.getElementById("colorPickerContainer");
+let spanelement = document.getElementById("selectedColorHexCode");
+
+function button1() {
+    document.getElementById("button1").style.backgroundColor = "#e0e0e0";
+    colorpicker.style.backgroundColor = "#e0e0e0";
+    spanelement.textContent = "#e0e0e0";
+}
+
+function button2() {
+    document.getElementById("button2").style.backgroundColor = "#6fcf97";
+    colorpicker.style.backgroundColor = "#6fcf97";
+    spanelement.textContent = "#6fcf97";
+}
+
+function button3() {
+    document.getElementById("button3").style.backgroundColor = "#56ccf2";
+    colorpicker.style.backgroundColor = "#56ccf2";
+    spanelement.textContent = "#56ccf2";
+}
+
+function button4() {
+    document.getElementById("button4").style.backgroundColor = "#bb6bd9";
+    colorpicker.style.backgroundColor = "#bb6bd9";
+    spanelement.textContent = "#bb6bd9";
+}
